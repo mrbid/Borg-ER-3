156852cfb1a964b02e27d04d731978364e4be9ab
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2021-09-12 22:44:45 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-gcc -std=c17 main.c -ISDL2/include -LSDL2/lib -Wall -lmingw32 -lSDL2main -lSDL2 -o borg.exe
+gcc -std=c17 main.c -ISDL2/include -LSDL2/lib -Ofast -Wall -lmingw32 -lSDL2main -lSDL2 -o borg.exe
