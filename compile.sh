3bf0ae0834c2ff4ce6836479eae8f5a5e3efbd4c
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2023-01-31 03:08:46 +0000

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,18 +1,26 @@
 mkdir release
-rm release/borg
-rm release/borg.exe
-rm release/borg.AppImage
-rm release/borg.deb
-rm release/borg_win.zip
+rm -f release/borg
+rm -f release/borg_native
+rm -f release/borg.exe
+rm -f release/borg.AppImage
+rm -f release/borg.deb
+rm -f release/borg_win.zip
 clang main.c -Ofast -lSDL2 -lm -o release/borg
+clang main.c -march=native -Ofast -lSDL2 -lm -o release/borg_native
 i686-w64-mingw32-gcc -std=c17 main.c -ISDL2/include -LSDL2/lib -Ofast -Wall -lmingw32 -lSDL2main -lSDL2 -o release/borg.exe
 strip --strip-unneeded release/borg
+strip --strip-unneeded release/borg_native
 strip --strip-unneeded release/borg.exe
 upx --lzma --best release/borg
+upx --lzma --best release/borg_native
 upx --lzma --best release/borg.exe
 cp release/borg borg.AppDir/usr/bin/borg
 ./appimagetool-x86_64.AppImage borg.AppDir release/borg.AppImage
 cp release/borg deb/usr/bin/borg
 dpkg-deb --build deb release/borg.deb
+cp SDL2/bin/SDL2.dll release/SDL2.dll
+strip --strip-unneeded release/SDL2.dll
+upx --lzma --best release/SDL2.dll
 cd release
 zip borg_win.zip SDL2.dll borg.exe
+rm -f SDL2.dll
