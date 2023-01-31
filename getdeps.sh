f27a52ef658456817e0a2ce9f327062a5c36f923
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2023-01-31 03:48:07 +0000

    Update getdeps.sh

--- getdeps.sh
+++ getdeps.sh
@@ -1,5 +1,5 @@
 rm -f appimagetool-x86_64.AppImage
-apt install libsdl2-2.0-0 libsdl2-dev gcc-mingw-w64-i686-win32 upx-ucl zip wget
+apt install libsdl2-2.0-0 libsdl2-dev gcc-mingw-w64-i686-win32 upx-ucl zip tar wget
 wget https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage
 chmod 0755 appimagetool-x86_64.AppImage
 mkdir SDL2
